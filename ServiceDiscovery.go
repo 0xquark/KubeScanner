77a021eea5741f074286c293ef9c7ad762fc90e0
Karanjot Singh <drquark@duck.com>
2023-03-23 03:24:04 +0530

    Check Anonymous access in etcd server
    Signed-off-by: Karanjot Singh <drquark@duck.com>

--- ServiceDiscovery.go
+++ ServiceDiscovery.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"time"
@@ -70,7 +71,7 @@ func discoverService(ip string, port int) string {
 	if isHTTP(ip, port) {
 		return "http"
 	}
-	if isETCD(ip, port) {
+	if isEtcd(ip, port) {
 		return "etcd"
 	}
 	// Add more discovery functions here
@@ -106,19 +107,50 @@ func isHTTP(ip string, port int) bool {
 }
 
 // Check if a port is serving ETCD
-func isETCD(ip string, port int) bool {
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", ip, port))
+func isEtcd(ip string, port int) bool {
+	// Attempt to connect to the etcd service
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", ip, port), time.Second*5)
 	if err != nil {
 		return false
 	}
 	defer conn.Close()
 
-	fmt.Fprintf(conn, "GET /version HTTP/1.0\r\n\r\n")
+	// Send a request to the etcd service
+	fmt.Fprintf(conn, "GET /version HTTP/1.1\r\nHost: %s\r\n\r\n", ip)
+
+	// Read the response from the etcd service
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 	if err != nil {
 		return false
 	}
 
-	return strings.Contains(string(buf[:n]), "etcdserver")
+	// Check if the response contains the etcd version string
+	isEtcd := strings.Contains(string(buf[:n]), "etcdserver")
+
+	if isEtcd {
+		// Attempt to access etcd using etcdctl
+		cmd := exec.Command("etcdctl", "--endpoints=http://"+ip+":2379", "get", "/", "--prefix", "--keys-only")
+		output, err := cmd.CombinedOutput()
+
+		// Check if etcdctl output indicates that anonymous access is available
+		isVulnerable := false
+		if err != nil {
+			if strings.Contains(string(output), "authorization failed") {
+				isVulnerable = true
+			}
+		} else {
+			if strings.TrimSpace(string(output)) != "" {
+				isVulnerable = true
+			}
+		}
+
+		if isVulnerable {
+			fmt.Printf("Etcd service on %s:%d is vulnerable to anonymous access\n", ip, port)
+		}
+
+		return isVulnerable
+	}
+
+	return false
 }
